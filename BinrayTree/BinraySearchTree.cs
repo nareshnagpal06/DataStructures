d3900dcd730626c0f8ab74d649ffa17fe8e35b3a
Naresh Nagpal <nanagpa@microsoft.com>
2020-02-13 22:02:56 +0530

    Added Binrary Search Tree Initial Draft

---
+++ BinrayTree/BinraySearchTree.cs
@@ -0,0 +1,37 @@
+
+namespace DS
+{
+    using System;
+    public class BinarySearchTree
+    {
+
+        Node rootNode;
+
+        public void Insert(string value)
+        {
+
+        }
+
+        public bool find(string value)
+        {
+            if (rootNode.Value==value)
+        }
+    }
+
+    private class Node
+    {
+        private string value;
+        private Node leftChild;
+
+        private Node rightChild;
+
+        public Node(string value)
+        {
+            this.value = value;
+        }
+
+        public string Value { get { return value; } }
+
+
+    }
+}
